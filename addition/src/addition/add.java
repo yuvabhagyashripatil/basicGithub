1805ff5425783adaf87c9160c01f7f0dc9b84425
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-02-04 13:57:34 +0530

    commit

---
+++ addition/src/addition/add.java
@@ -0,0 +1,13 @@
+package addition;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 =5,num2 =15,sum;
+		sum=num1+num2;
+		System.out.println("sum of these numbers:"+sum);
+
+	}
+
+}
