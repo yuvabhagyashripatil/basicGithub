6c111ebef4112ab3a8775be5195e93b571375916
bhagyashri <yuvabhagyashripatil@gmail.com>
2022-02-04 14:26:39 +0530

    commit

--- addition/src/addition/add.java
+++ addition/src/addition/add.java
@@ -4,8 +4,8 @@ public class add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1 =5,num2 =15,sum;
-		sum=num1+num2;
+		int num1 =5,num2 =15,num3=7,sum;
+		sum=num1+num2+num3;
 		System.out.println("sum of these numbers:"+sum);
 
 	}
